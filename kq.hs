70c55c607c73030dc246a5530f4729b6df899ae3
Fumiaki Kinoshita <fumiexcel@gmail.com>
2011-10-30 17:24:34 +0900

    refactored

--- kq.hs
+++ kq.hs
@@ -1,23 +1,28 @@
+{-
+LazyKQ interpreter
+   -}
 
 import Control.Applicative
 import Data.Char
 import System
 
-data Expr = Apply Expr Expr
+data Expr = App Expr Expr
         | I
         | K | K' Expr
         | S | S' Expr | S'' Expr Expr
-        | Increment
+        | Inc
         | Export Int
 
 instance Show Expr where
-    show (Apply a b) = "ダァ" ++ show a ++ show b 
+    show (App a b) = "ダァ" ++ show a ++ show b 
     show S = "シエリ"
     show K = "イェ"
     show I = "ス"
     show (S' a) = "ダァシエリ" ++ show a
     show (K' a) = "ダァイェ" ++ show a
     show (S'' a b) = "ダァダァシエリ" ++ show a ++ show b
+    show Inc = "+"
+    show (Export x) = show x
 
 apply :: Expr -> Expr -> Expr
 apply I x = x
@@ -25,72 +30,75 @@ apply K x = K' x
 apply (K' x) y = x
 apply S x = S' x
 apply (S' x) y = S'' x y
-apply (S'' x y) z = apply (apply x z) (apply y z)
-apply Increment (Export x) = Export $ x + 1
+apply (S'' x y) z = apply x z `apply` apply y z
+apply Inc (Export x) = Export $ x + 1
 apply f x = error $ "Can't apply " ++ show f ++ " to " ++ show x
---apply f x = apply (eval f) (eval x)
 
 eval :: Expr -> Expr
-eval (Apply x y) = apply (eval x) (eval y)
+eval (App x y) = eval x `apply` eval y
 eval x = x
 
+export :: Expr -> Maybe Int
 export (Export x) = Just x
 export _ = Nothing
 
-parse :: String -> (Expr, String)
+parseKQ :: String -> (Expr, String)
 
-parse ('ダ':'ァ':xs) = let (a0, xs') = parse xs in
-                         let (a1, xs'') = parse xs' in
-                             (Apply a0 a1, xs'')
-parse ('ダ':'ア':xs) = parse ('ダ':'ァ':xs)
+parseKQ ('ダ':'ァ':xs) = let (a0, xs') = parseKQ xs in
+                         let (a1, xs'') = parseKQ xs' in
+                             (App a0 a1, xs'')
+parseKQ ('ダ':'ア':xs) = parseKQ ('ダ':'ァ':xs)
+parseKQ ('シ':'エ':'リ':xs) = (S, xs)
+parseKQ ('シ':'ェ':'リ':xs) = (S, xs)
+parseKQ ('イ':'ェ':xs) = (K, xs)
+parseKQ ('イ':'エ':xs) = (K, xs)
+parseKQ ('ス':xs) = (I, xs)
+parseKQ ('#':'\n':xs) = parseKQ xs
+parseKQ ('#':_:xs) = parseKQ ('#':xs)
+parseKQ (_:xs) = parseKQ xs
+parseKQ "" = (I, "")
 
-parse ('シ':'エ':'リ':xs) = (S, xs)
-parse ('シ':'ェ':'リ':xs) = (S, xs)
+parse = fst.parseKQ
 
-parse ('イ':'ェ':xs) = (K, xs)
-parse ('イ':'エ':xs) = (K, xs)
+cons :: Expr -> Expr -> Expr
+cons a b = App (App S (App (App S I) (App K a))) (App K b)
 
-parse ('ス':xs) = (I, xs)
+church :: Int -> Expr
+church 0 = App K I
+church n = App (App S (App (App S (App K S)) K)) (church $ n - 1)
 
-parse x = error $ x ++ ": Syntax Error"
+encode :: String -> Expr
+encode = foldr cons (church 256) . map (church.ord)
 
-ignoreCharacters = " \n!！"
+realize :: Expr -> Maybe Int
+realize expr = export $ eval $ App (App expr Inc) $ Export 0
 
-church 0 = Apply K I
-church n = Apply (Apply S (Apply (Apply S (Apply K S)) K)) (church $ n - 1)
+decode :: Expr -> (String, Int)
+decode expr = case realize $ App expr K of
+     Nothing -> ("", 1)
+     Just x -> if x >= 256
+                  then ("", x - 256)
+                  else (chr x : a, b) where
+                      (a, b) = decode $ eval $ App expr (App K I)
 
-cons a b = Apply (Apply S (Apply (Apply S I) (Apply K a))) (Apply K b)
+output :: (String, Int) -> IO ()
+output (a, b) = putStr a >> exitWithCode b
 
-realize expr = export $ eval $ Apply (Apply expr Increment) (Export 0)
+exitWithCode 0 = exitWith $ ExitSuccess
+exitWithCode n = exitWith $ ExitFailure n
 
-formCode = filter (not.(flip elem) ignoreCharacters)
+interpret r d xs = (if r then print else output.decode).eval =<< if d
+    then parse <$> getContents
+    else App <$> parse <$> readFile (head xs) <*> (encode <$> getContents)
 
-output expr = case realize (Apply expr K) of
-        Nothing -> return ()
-        Just 256 -> exitWith ExitSuccess
-        Just x -> do
-            if x >= 256
-               then exitWith $ ExitFailure $ x - 256
-               else do
-                   putChar $ chr x
-                   output $ eval $ Apply expr (Apply K I)
+main = do args <- getArgs
+          if "-h" `elem` args
+             then help
+             else interpret ("-r" `elem` args) ("-d" `elem` args)
+                 $ filter (('-'/=).head) args
 
-main = do
-    args <- getArgs
-    opts <- return $ filter (('-'==).head) args
-    if "-h" `elem` opts
-       then do
-           putStrLn "Usage: kq [options] [filename]"
-           putStrLn "-r: show as an expression"
-           putStrLn "-d: read the source from stdin"
-           putStrLn "-h: show this help"
-       else do
-           printer <- return (if "-r" `elem` opts then print else output)
-           if  "-d" `elem` opts
-              then do
-                code <- formCode <$> getContents
-                printer $ eval $ fst $ parse code
-              else do
-                input <- foldr cons (church 256) <$> map (eval.church.ord) <$>  getContents
-                code <- formCode <$> (readFile $ head $ filter (('-'/=).head) args)
-                printer $ eval $ Apply (fst $ parse code) $ input
+help = do
+    putStrLn "Usage: kq [options] [filename]"
+    putStrLn "-r: show as an expression"
+    putStrLn "-d: read a source from stdin"
+    putStrLn "-h: show this help"
